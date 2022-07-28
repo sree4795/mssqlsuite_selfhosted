8f7f1642c56617fe63c76916c14635b50bbaeaac
srinivasareddy4218 <63068380+srinivasareddy4218@users.noreply.github.com>
2022-07-28 17:28:30 +0530

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -8,64 +8,7 @@ param (
 
 if ("sqlengine" -in $Install) {
    Write-Output "Installing SQL Engine"
-   if ($ismacos) {
-      Write-Output "mac detected, installing docker then downloading a docker container"
-      $Env:HOMEBREW_NO_AUTO_UPDATE = 1
-      brew install --cask docker
-      sudo /Applications/Docker.app/Contents/MacOS/Docker --unattended --install-privileged-components
-      open -a /Applications/Docker.app --args --unattended --accept-license
-      Start-Sleep 30
-      $tries = 0
-      Write-Output "We are waiting for Docker to be up and running. It can take over 2 minutes..."
-      do { 
-         try {
-            $tries++
-            $sock = Get-ChildItem $home/Library/Containers/com.docker.docker/Data/docker.raw.sock -ErrorAction Stop
-         } catch {
-            Write-Output "Waiting..."
-            Start-Sleep 5
-         }
-      }
-      until ($sock.BaseName -or $tries -gt 55)
-      
-      if ($tries -gt 55) {
-         Write-Output "
-         
-         
-         
-         Moving on without waiting for docker to start
-         
-         
-         
-         
-         "
-      }
-
-      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -e "MSSQL_COLLATION=$Collation" --name sql -p 1433:1433 --memory="2g" -d mcr.microsoft.com/mssql/server:2019-latest
-      Write-Output "Docker finished running"
-      Start-Sleep 5
-      if ($ShowLog) {
-         docker ps -a
-         docker logs -t sql
-      }
-      
-      Write-Output "sql engine installed at localhost"
-   }
-
-   if ($islinux) {
-      Write-Output "linux detected, downloading the 2019 docker container"
-      docker run -e "ACCEPT_EULA=Y" -e "SA_PASSWORD=$SaPassword" -e "MSSQL_COLLATION=$Collation" --name sql -p 1433:1433 -d mcr.microsoft.com/mssql/server:2019-latest
-      Write-Output "Waiting for docker to start"
-      Start-Sleep -Seconds 10
-      
-      if ($ShowLog) {
-         docker ps -a
-         docker logs -t sql
-      }
-      Write-Output "docker container running - sql server accessible at localhost"
-   }
-
-   if ($iswindows) {
+   
       Write-Output "windows detected, downloading sql server 2019"
       # docker takes 16 minutes, this takes 5 minutes
       if (-not (Test-Path C:\temp)) {
@@ -85,72 +28,5 @@ if ("sqlengine" -in $Install) {
       
       Write-Output "sql server 2019 installed at localhost and accessible with both windows and sql auth"
    }
-}
 
-if ("sqlclient" -in $Install) {
-   if ($ismacos) {
-      Write-Output "Installing sqlclient tools"
-      brew tap microsoft/mssql-release https://github.com/Microsoft/homebrew-mssql-release
-      $null = brew update
-      $log = brew install msodbcsql17 mssql-tools
-      
-      if ($ShowLog) {
-         $log
-      }
-   }
-   
-   Write-Output "sqlclient tools are installed"
-}
 
-if ("sqlpackage" -in $Install) {
-   Write-Output "installing sqlpackage"
-
-   if ($ismacos) {
-      curl "https://go.microsoft.com/fwlink/?linkid=2143659" -4 -sL -o '/tmp/sqlpackage.zip'
-      $log = unzip /tmp/sqlpackage.zip -d $HOME/sqlpackage
-      chmod +x $HOME/sqlpackage/sqlpackage
-      sudo ln -sf $HOME/sqlpackage/sqlpackage /usr/local/bin
-      if ($ShowLog) {
-         $log
-         sqlpackage /version
-      }
-   }
-
-   if ($islinux) {
-      curl "https://go.microsoft.com/fwlink/?linkid=2143497" -4 -sL -o '/tmp/sqlpackage.zip'
-      $log = unzip /tmp/sqlpackage.zip -d $HOME/sqlpackage
-      chmod +x $HOME/sqlpackage/sqlpackage
-      sudo ln -sf $HOME/sqlpackage/sqlpackage /usr/local/bin
-      if ($ShowLog) {
-         $log
-         sqlpackage /version
-      }
-   }
-
-   if ($iswindows) {
-      $log = choco install sqlpackage
-      if ($ShowLog) {
-         $log
-         sqlpackage /version
-      }
-   }
-   
-   Write-Output "sqlpackage installed"
-}
-
-if ("localdb" -in $Install) {
-   if ($iswindows) {
-      Write-Host "Downloading SqlLocalDB"
-      $ProgressPreference = "SilentlyContinue"
-      Invoke-WebRequest -Uri https://download.microsoft.com/download/7/c/1/7c14e92e-bdcb-4f89-b7cf-93543e7112d1/SqlLocalDB.msi -OutFile SqlLocalDB.msi
-      Write-Host "Installing"
-      Start-Process -FilePath "SqlLocalDB.msi" -Wait -ArgumentList "/qn", "/norestart", "/l*v SqlLocalDBInstall.log", "IACCEPTSQLLOCALDBLICENSETERMS=YES";
-      Write-Host "Checking"
-      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "SELECT @@VERSION;"
-      sqlcmd -S "(localdb)\MSSQLLocalDB" -Q "ALTER LOGIN [sa] WITH PASSWORD=N'$SaPassword'"
-      
-      Write-Host "SqlLocalDB installed and accessible at (localdb)\MSSQLLocalDB"
-   } else {
-      Write-Output "localdb cannot be isntalled on mac or linux"
-   }
-}
